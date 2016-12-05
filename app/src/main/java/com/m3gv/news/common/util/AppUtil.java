fcfb9ea6e8e2d6860b8aa1b7b9fdd3085122a97a
meikai <meikai@mucang.cn>
2016-12-05 21:27:29 +0800

    修改重播按钮，删除无用so

---
+++ app/src/main/java/com/m3gv/news/common/util/AppUtil.java
@@ -0,0 +1,110 @@
+package com.m3gv.news.common.util;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.text.TextUtils;
+
+import java.util.List;
+
+/**
+ * Created by meikai on 15/10/16.
+ */
+public class AppUtil {
+
+    private AppUtil() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+
+    }
+
+
+    /**
+     * 获取进程名称
+     *
+     * @param context 上下文
+     * @return 进程名称
+     */
+    public static String getProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        if (infos != null) {
+            for (ActivityManager.RunningAppProcessInfo processInfo : infos) {
+                if (processInfo.pid == pid) {
+                    return processInfo.processName;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 获取应用程序名称
+     */
+    public static String getAppName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            int labelRes = packageInfo.applicationInfo.labelRes;
+            return context.getResources().getString(labelRes);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * [获取应用程序版本名称信息]
+     *
+     * @param context
+     * @return 当前应用的版本名称
+     */
+    public static String getVersionName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            return packageInfo.versionName;
+
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * 检测应用是否运行
+     *
+     * @param packageName 包名
+     * @param context     上下文
+     * @return 是否存在
+     */
+    public static boolean isAppAlive(String packageName, Context context) {
+        if (context == null || TextUtils.isEmpty(packageName)) {
+            return false;
+        }
+
+        ActivityManager activityManager = (ActivityManager)
+                context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        if (activityManager != null) {
+            List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+            if (procInfos != null && !procInfos.isEmpty()) {
+                for (int i = 0; i < procInfos.size(); i++) {
+                    if (procInfos.get(i).processName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
